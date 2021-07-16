993afef795cce2b4f4bd1a40e864c7d9e8f5ab35
Emmanuel Egomson <emmanuelegomson@E-Egomson.attlocal.net>
2021-07-16 12:16:19 -0500

    Complete feature section design

--- client/src/components/featured/Featured.js
+++ client/src/components/featured/Featured.js
@@ -3,9 +3,32 @@ import './Featured.scss'
 import PlayArrowIcon from '@material-ui/icons/PlayArrow';
 import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined';
 
-const Featured = () => {
+const Featured = ({ type }) => {
   return (
     <div className="featured">
+      {
+        type && (
+          <div className="category">
+            <span>{ type === 'movie' ? 'Movies' : 'TV Series' }</span>
+            <select name="genre" id="genre">
+              <option>Genre</option>
+              <option value="adventure">Adventure</option>
+              <option value="comedy">Comedy</option>
+              <option value="crime">Crime</option>
+              <option value="fantasy">Fantasy</option>
+              <option value="historical">Historical</option>
+              <option value="horror">Horror</option>
+              <option value="romance">Romance</option>
+              <option value="sci-fi">Sci-fi</option>
+              <option value="thriller">Thriller</option>
+              <option value="western">Western</option>
+              <option value="animation">Animation</option>
+              <option value="drama">Drama</option>
+              <option value="documentary">Documentary</option>
+            </select>
+          </div>
+        )
+      }
       <img src="https://bit.ly/3xMj9KC" alt="" width="100%"/>
       <div className="info">
         <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/96/FOX_wordmark-orange.svg/1200px-FOX_wordmark-orange.svg.png" alt="" />
